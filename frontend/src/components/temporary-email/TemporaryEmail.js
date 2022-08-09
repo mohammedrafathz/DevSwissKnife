753edd18fc1253700d7e0bb9df7ab997d433ea1b
AnirudhAllani115 <c0818115@mylambton.ca>
2022-08-09 19:49:18 -0400

    temporary email

--- frontend/src/components/temporary-email/TemporaryEmail.js
+++ frontend/src/components/temporary-email/TemporaryEmail.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { Container } from 'reactstrap';
-
+/* eslint-disable no-unused-vars */
+import React, {useCallback, useState} from 'react';
+import {Button, Container, Label} from 'reactstrap';
 
 const TemporaryEmail = () => {
-  //TODO auto detect json from clipboard and show as placeholder
-  <div>
-    <Container>
-      <h1 className='text-center'>TEMPORARY EMAIL</h1>
-    </Container>
-  </div>;
+  const [email, setemail] = useState('');
 
-};
+  return (
+    <>
+      <Container>
+        <h1 className='text-center'>TEMPORARY EMAIL {email}</h1>
+        <Label style={{marginLeft:'15%', marginTop:'5%'}}>This feature is still under development. </Label>
+        <Label> Please visit this site for the same <a href="https://tempmail.email/"> TemporaryEmail</a>.</Label>
+      </Container>
+    </>
+  );
+}
+  ;
 
 export default TemporaryEmail;
